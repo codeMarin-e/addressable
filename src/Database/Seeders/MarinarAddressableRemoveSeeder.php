5fab89dd48780afca80f4ad440661fc46a993d91
Marin <marin@dev.nddesign.no>
2023-01-23 09:56:23 +0200

    logic change

--- src/Database/Seeders/MarinarAddressableRemoveSeeder.php
+++ src/Database/Seeders/MarinarAddressableRemoveSeeder.php
@@ -3,19 +3,23 @@
 
     use Illuminate\Database\Seeder;
     use App\Models\Address;
+    use Marinar\Addressable\MarinarAddressable;
 
     class MarinarAddressableRemoveSeeder extends Seeder {
+
         use \Marinar\Marinar\Traits\MarinarSeedersTrait;
 
         public function run() {
-            $this->getRefComponents();
-            $this->clearDBrows();
-            $this->clearFiles();
-            $this->dbMigrateRollback();
+            if(!in_array(env('APP_ENV'), ['dev', 'local'])) return;
+            static::$packageName = 'marinar_addressable';
+            static::$packageDir = MarinarAddressable::getPackageMainDir();
+
+            $this->autoRemove();
+
             $this->refComponents->info("Done!");
         }
 
-        public function clearDBrows() {
+        public function clearDB() {
             $this->refComponents->task("Clear DB rows", function() {
                 $addresses = Address::get();
                 foreach ($addresses as $address) {
@@ -23,21 +27,5 @@
                 }
                 return true;
             });
-        }
-
-        private function clearFiles() {
-//            if(!$this->command->confirm('Are you sure you want to delete `addressable` files?', false)) return false;
-            $this->refComponents->task("Clear stubs", function() {
-                $copyDir = \Marinar\Addressable\MarinarAddressable::getPackageMainDir().DIRECTORY_SEPARATOR.'stubs';
-                static::removeStubFiles($copyDir, $copyDir, true);
-                return true;
-            });
-        }
-
-        private function dbMigrateRollback() {
-            $this->dbMigrateRollbackDir(implode(DIRECTORY_SEPARATOR, [
-                \Marinar\Addressable\MarinarAddressable::getPackageMainDir(),
-                'stubs', 'project', 'database', 'migrations',
-            ]));
         }
     }
